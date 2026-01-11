c70ac437d8be275ca8e8277f47c6d33c9ec48a5c
allen <allenmuhani@gmail.com>
2026-01-11 16:37:54 +0300

    tested update of new base points

--- test/TestTokenizedVaultTokenDetails.t.sol
+++ test/TestTokenizedVaultTokenDetails.t.sol
@@ -6,19 +6,29 @@ import {Test} from "forge-std/Test.sol";
 import {TokenizedVault} from "../src/TokenizedVault.sol";
 
 contract TestTokenizedVaultTokenDetails is Test {
-    function test_TokenDetails() public {
-        // Deploy the TokenizedVault contract
-        address mockAsset = address(0x1234567890123456789012345678901234567890);
-        uint256 feeBasisPoints = 500; // 5% fee in basis points
-        string memory name = "Tokenized Vault Ocean Token";
-        string memory symbol = "tvOCT";
+    TokenizedVault vault;
 
-        TokenizedVault vault = new TokenizedVault(mockAsset, feeBasisPoints, name, symbol);
+    address mockAsset = address(0x1234567890123456789012345678901234567890);
+    uint256 feeBasisPoints = 500; // 5% fee in basis points
+    string name = "Tokenized Vault Ocean Token";
+    string symbol = "tvOCT";
 
+    function setUp() external {
+        vault = new TokenizedVault(mockAsset, feeBasisPoints, name, symbol);
+    }
+
+    function test_TokenDetails() external view {
         // Verify the token details
         assertEq(vault.name(), name);
         assertEq(vault.symbol(), symbol);
         assertEq(vault.asset(), mockAsset);
         assertEq(vault.getFeeBasePoints(), feeBasisPoints);
+        assertEq(vault.vaultOwner(), address(this));
+    }
+
+    function test_UpdateFeeBasePoints() public {
+        uint256 newFeeBasisPoints = 300; // 3% fee in basis points
+        vault.updateFeeBasePoints(newFeeBasisPoints);
+        assertEq(vault.getFeeBasePoints(), newFeeBasisPoints);
     }
 }
